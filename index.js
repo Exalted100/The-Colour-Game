874233d8192362c6a1906b10d21b15fd3f251a21
Fawaz Haroun <harounayomikun@gmail.com>
2021-01-18 20:18:32 +0100

    update

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector(".three");
+const four = document.querySelector(".four");
 
-one.style.backgroundColor = "red";
+const backgroundColours = [
+    `rgb(250, 250, 0)`,
+    "rgb(0, 250, 0)",
+    "rgb(0, 0, 250)",
+    "rgb(0, 250, 250)",
+]
 
-const changeColour = () => {
-    one.style.backgroundColor = "blue";
+const rightColour = backgroundColours[2]
+
+document.querySelector(".colour-code").textContent = rightColour;
+
+for (let i = 0; i < 4; i++) {
+    document.querySelectorAll(".colour")[i].style.backgroundColor = backgroundColours[i];
 }
 
-one.addEventListener("click", changeColour);
+const squareClick = (chosen) => {
+    const chosenColour = chosen.style.background;
+
+    if (chosenColour === rightColour) {
+        alert("You're right!");
+    } else {
+        alert("Oops, that isn't the right colour. Try again.")
+    }
+
+    console.log(chosenColour + " " + rightColour)
+}
+
+one.addEventListener("click", () => squareClick(one));
+two.addEventListener("click", () => squareClick(two));
+three.addEventListener("click", () => squareClick(three));
+four.addEventListener("click", () => squareClick(four));
